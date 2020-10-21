3305a5f32a96657f817f25f89de8ac26fbf3200b
Matthew Deweese <Tiptup300@gmail.com>
2020-10-20 23:01:21 -0400

    init

---
+++ 2020-10-09-- test-making-html-table-engine/Class1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_library_table
+{
+    public class Table
+    {
+        public List<Column> Columns = new List<Column>();
+        public List<Row> Rows = new List<Row>();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<html><head><style>* {padding: 0px 26px;border: 1px solid black;}</style></head><body><table>");
+            sb.AppendLine("<table>");
+            sb.AppendLine($"<thead>");
+            sb.AppendLine($"</thead>");
+            sb.AppendLine($"<tbody>");
+
+            foreach (Row r in Rows)
+                sb.AppendLine(r.ToString());
+
+            sb.AppendLine($"</tbody>");
+            sb.AppendLine($"</table>");
+            sb.AppendLine("</body></html>");
+
+            return sb.ToString();
+        }
+    }
+
+    public class Row
+    {
+        public List<Cell> Cells = new List<Cell>();
+        public List<Property> Properties { get; set; } = new List<Property>();
+
+        public Row()
+        {
+
+        }
+
+        public Row(params Property[] properties)
+        {
+            Properties.AddRange(properties);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"<tr>");
+            foreach (Cell c in Cells)
+                sb.AppendLine(c.ToString());
+            sb.AppendLine($"</tr>");
+
+            return sb.ToString();
+        }
+    }
+
+    public class Column : Cell
+    {
+        public Column(string text, params Property[] properties) : base(text, text, properties) 
+        {
+            ElementType = "hr";
+        }
+    }
+
+    public class Cell
+    {
+        public string ElementType { get; set; } = "td";
+        public string Text { get; set; }
+        public object Data { get; set; }
+        public Properties Properties { get; set; } = new Properties();
+
+        public Cell(string text, params Property[] properties) : this(text, text, properties) { }
+
+        public Cell(string text, object data, params Property[] properties)
+        {
+            Text = text;
+            Data = data;
+            ElementType = "td";
+            Properties.AddRange(properties);
+        }
+
+        public override string ToString()
+        {
+            return $"<{ ElementType } { Properties.ToString() } >{ Text }</{ ElementType }>";
+        }
+    }
+
+    public class Properties : List<Property>
+    {
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var p in this)
+            {
+                sb.Append(p.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public class Property
+    {
+        public string Identifier { get; set; }
+        public string Value { get; set; }
+
+        public Property(string identifier, string value)
+        {
+            Identifier = identifier;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{ Identifier }=\"{ Value }\"";
+        }
+    }
+
+    public static class StyleProperties
+    {
+        public static StyleProperty AlignLeft(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignLeft);
+        }
+
+        public static StyleProperty AlignCenter(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignCenter);
+        }
+
+        public static StyleProperty AlignRight(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignRight);
+        }
+
+        public static StyleProperty Width(this StyleProperty sp, string value)
+        {
+            return sp.Add($"width: { value }");
+        }
+        public static StyleProperty Bold(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.Bold);
+        }
+
+        public static StyleProperty AlignTop(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignTop);
+        }
+
+        public static StyleProperty AlignMiddle(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignMiddle);
+        }
+
+        public static StyleProperty AlignBottom(this StyleProperty sp)
+        {
+            return sp.Add(StyleProperty.AlignBottom);
+        }
+    }
+
+    public class StyleProperty : Property
+    {
+        public static StyleProperty AlignLeft { get { return new StyleProperty("text-align:left;"); } }
+        public static StyleProperty AlignCenter { get { return new StyleProperty("text-align:center;"); } }
+        public static StyleProperty AlignRight { get { return new StyleProperty("text-align:right;"); } }
+        public static StyleProperty Bold { get { return new StyleProperty("font-weight:bold;"); } }
+        public static StyleProperty AlignTop { get { return new StyleProperty("vertical-alignment:top;"); } }
+        public static StyleProperty AlignMiddle { get { return new StyleProperty("vertical-alignment:middle;"); } }
+        public static StyleProperty AlignBottom { get { return new StyleProperty("vertical-alignment:bottom;"); } }
+        public StyleProperty(string styleParameters) : base("style",styleParameters) { }
+
+        public StyleProperty Add(string additionalValue)
+        {
+            Value += $"{ additionalValue };";
+            return this;
+        }
+
+        public StyleProperty Add(StyleProperty additionalStyleProperty)
+        {
+            Value += $"{ additionalStyleProperty.Value };";
+            return this;
+        }
+    }
+
+    public class ColSpanProperty : Property
+    {
+
+        public ColSpanProperty(string number) : base("colspan", number) { }
+        public ColSpanProperty(int number) : base("colspan", number.ToString()) { }
+    }
+
+    public class RowSpanProperty : Property
+    {
+        public RowSpanProperty(string number) : base("rowspan", number) { }
+        public RowSpanProperty(int number) : base("rowspan", number.ToString() ) { }
+    }
+
+    public class ClassProperty : Property
+    {
+        public ClassProperty(string className) : base("class", className) { }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test_library_table;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Table t = new Table();
+
+            int bidNumber = 7;
+            string bidName = "BIG OLD BID";
+
+            t.Columns.Add(new Column("Building Number", StyleProperty.AlignLeft.Width("48pt"), new ColSpanProperty(1)));
+            t.Columns.Add(new Column("Building Name", StyleProperty.AlignLeft.Width("108pt"), new ColSpanProperty(1)));
+            t.Columns.Add(new Column("Requestor Name", StyleProperty.AlignLeft.Width("108pt"), new ColSpanProperty(1)));
+            t.Columns.Add(new Column("Total Quantity", StyleProperty.AlignRight.Width("48pt"), new ColSpanProperty(1)));
+            t.Columns.Add(new Column("Total Extended Price", StyleProperty.AlignRight.Width("72pt"), new ColSpanProperty(1)));
+
+
+            decimal Total_totalQuantity = 0, Total_finalTotal = 0;
+
+            for (int x = 0; x < 10; x++)
+            {
+                Row r = new Row();
+
+                int buildingNumber = 7;
+                string buildingName = "Sam Elliots Building";
+                string requestorName = "Frankie ELEM";
+                decimal totalQuantity = 4.5M;
+                decimal finalTotal = 3700.37234M;
+
+
+                // Top Row
+                r.Cells.Add(new Cell(buildingNumber.ToString("00"), buildingNumber, StyleProperty.AlignLeft.Width("48pt").Bold(),new RowSpanProperty(3)));
+                r.Cells.Add(new Cell(buildingName, StyleProperty.AlignLeft.Width("108pt").Bold(), new RowSpanProperty(3)));
+                r.Cells.Add(new Cell(requestorName, StyleProperty.AlignLeft.Width("108pt").Bold()));
+                r.Cells.Add(new Cell(totalQuantity.ToString("0.0"), totalQuantity, StyleProperty.AlignRight.Width("48pt")));
+                r.Cells.Add(new Cell(finalTotal.ToString("$0.00"), finalTotal, StyleProperty.AlignRight.Width("72pt")));
+
+                Total_totalQuantity += totalQuantity;
+                Total_finalTotal += finalTotal;
+
+                t.Rows.Add(r);
+
+
+                decimal subTotal_totalQuantity = 0, subTotal_finalTotal = 0;
+
+                for (int y = 1; y < 3; y++)
+                {
+                    // Middle Row
+                    Row subRow = new Row();
+                    subRow.Cells.Add(new Cell(requestorName, StyleProperty.AlignLeft.Width("108pt").Bold()));
+                    subRow.Cells.Add(new Cell(totalQuantity.ToString("0.0"), totalQuantity, StyleProperty.AlignRight.Width("48pt")));
+                    subRow.Cells.Add(new Cell(finalTotal.ToString("$0.00"), finalTotal, StyleProperty.AlignRight.Width("72pt")));
+
+                    subTotal_totalQuantity += totalQuantity;
+                    subTotal_finalTotal += finalTotal;
+
+                    t.Rows.Add(subRow);
+                }
+
+                Row subTotalRow = new Row(new ClassProperty("subtotals"));
+                subTotalRow.Cells.Add(new Cell($"Total: { buildingNumber.ToString("00") } { buildingName }", StyleProperty.AlignRight.Bold(), new ColSpanProperty(3)));
+                subTotalRow.Cells.Add(new Cell(subTotal_totalQuantity.ToString("0.0"), subTotal_totalQuantity, StyleProperty.AlignRight.Bold()));
+                subTotalRow.Cells.Add(new Cell(subTotal_finalTotal.ToString("$0.00"), subTotal_finalTotal, StyleProperty.AlignRight.Bold()));
+
+                Total_totalQuantity += subTotal_totalQuantity;
+                Total_finalTotal += subTotal_finalTotal;
+
+                t.Rows.Add(subTotalRow);
+            }
+
+            // Total Row
+            Row totalRow = new Row(new ClassProperty("totals"));
+
+            totalRow.Cells.Add(new Cell($"Total: { bidNumber.ToString("00") } { bidName }", StyleProperty.AlignRight.Bold(), new ColSpanProperty(3)));
+            totalRow.Cells.Add(new Cell(Total_totalQuantity.ToString("0.0"), Total_totalQuantity, StyleProperty.AlignRight.Bold()));
+            totalRow.Cells.Add(new Cell(Total_finalTotal.ToString("$0.00"), Total_finalTotal, StyleProperty.AlignRight.Bold()));
+
+            t.Rows.Add(totalRow);
+
+            textBox1.Text = t.ToString();
+        }
+    }
+}
